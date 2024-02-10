0516e12545d27b5ca31d06e2ff708a7df399021c
rivercity310 <h970126@gmail.com>
2024-02-10 15:17:37 +0900

    feat: useReducer 훅 사용

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,58 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useReducer, useContext, useEffect, useRef, useCallback } from "react"
 
 
-
-const useWindowWidth = () => {
-  const [width, setWidth] = useState(window.innerWidth);
-  useEffect(() => {
-    const onResize = () => setWidth(window.innerWidth);
-    window.addEventListener('resize', onResize);
-    return () => {
-      window.removeEventListener('resize', onResize);
-    }
-  }, []);
-
-  return width;
+interface StateType {
+  name: string | undefined,
+  age: number | undefined
 }
 
-
-const useMounted = () => {
-  const [mounted, setMounted] = useState(false);
-  useEffect(() => {
-    setTimeout(() => setMounted(true), 1500);
-  }, []);
-  return mounted;
+interface ActionType {
+  type: Action,
+  name: string | undefined,
+  age: number | undefined
 }
 
+enum Action {
+  SetName = 'setName',
+  SetAge = 'setAge'
+}
+
+const INITIAL_STATE: StateType = { name: 'empty', age: 0 };
+function reducer(state: StateType, action: ActionType): StateType {
+  switch (action.type) {
+    case Action.SetName:
+      return { ...state, name: action.name };
+
+    case Action.SetAge:
+      return { ...state, age: action.age };
+
+    default:
+      return state;
+  }
+}
 
 const App = () => {
-  const width = useWindowWidth();
-  const mounted = useMounted();
+  const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
 
   return (
     <div>
-      {`mounted is ${mounted}`} <br/>
-      {`width is ${width}`}
+      <p>{`name is ${state.name}`}</p>
+      <p>{`age is ${state.age}`}</p>
+      <input
+        type="text"
+        value={state.name === undefined ? '' : state.name}
+        onChange={e => dispatch({ ...state, type: Action.SetName, name: e.target.value })}
+      />
+
+      <input 
+        type="number"
+        value={state.age === undefined ? 0 : state.age}
+        onChange={e => dispatch({ ...state, type: Action.SetAge, age: Number(e.target.value) })}
+      />
     </div>
   )
 }
+
 
 
 export default App
