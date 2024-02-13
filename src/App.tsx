509bd11a545690b3e5ec19bee694ed0c8dd1c3d6
rivercity310 <h970126@gmail.com>
2024-02-13 12:57:58 +0900

    Initialize Project

--- src/App.tsx
+++ src/App.tsx
@@ -1,108 +1,38 @@
-import React, { useState, useReducer, useContext, useEffect, useRef, useCallback } from "react"
-import { signin, signup, getAllUser, deleteUser } from "./api/user.api";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 
-interface User {
-  user_id: number,
-  user_email: string,
-  user_password: string
+/*
+* React, TypeScript, Vite, eslint, prettier 초기 세팅
+* https://velog.io/@navyjeongs/vite%EB%A1%9C-%EB%A6%AC%EC%95%A1%ED%8A%B8-%ED%94%84%EB%A1%9C%EC%A0%9D%ED%8A%B8-%EC%B4%88%EA%B8%B0-%EC%84%B8%ED%8C%85%ED%95%98%EA%B8%B0-1-with.-typescript-eslint-prettier-husky-%EC%A0%88%EB%8C%80%EA%B2%BD%EB%A1%9C 
+*/
+
+function App() {
+    const [count, setCount] = useState(0);
+    console.log('as');
+
+    return (
+        <>
+            <div>
+                <a href="https://vitejs.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img src={reactLogo} className="logo react" alt="React logo" />
+                </a>
+            </div>
+            <h1>Vite + React</h1>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+            </div>
+            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+        </>
+    );
 }
 
-
-const App = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [users, setUsers] = useState<User[]>([]);
-
-  const idRef = useRef<HTMLInputElement>(null);
-  const pwRef = useRef<HTMLInputElement>(null);
-
-  const signUp = (email: string, password: string) => {
-    signup(email, password)
-      ?.then(res => {
-        if (res.status == 200) {
-          localStorage.setItem('user_id', res.data['user_id']);
-          localStorage.setItem('user_email', res.data['user_email']);
-          localStorage.setItem('user_password', res.data['user_password']);
-        }
-        
-        const user = res.data;
-        setUsers([...users, { user_id: user.user_id, user_email: user.user_email, user_password: user.user_password}])
-      })
-  };
-
-  const deleteSpecificUser = (email: string, password: string) => {
-    deleteUser(email, password)
-      ?.then(res => {
-        if (res.status !== 200) {
-          throw Error("");
-        }
-
-        const deleted = res.data;
-        const remains = users.filter(user => user.user_id !== deleted.user_id);
-        setUsers(remains);
-      })
-      .catch(err => {
-        console.log(err);
-        return;
-      })
-  }
-
-  useEffect(() => {
-    getAllUser()
-    .then(res => {
-      if (res.status == 200) {
-        setUsers(res.data);
-      }
-    })
-    .catch(err => {
-      console.log(err);
-      const user_email = localStorage.getItem("user_email");
-      const user_password = localStorage.getItem("user_password");
-      
-      if (user_email !== null && user_password !== null) {
-        return signin(user_email, user_password);
-      }
-    })
-  }, []);
-
-  return (
-    <React.Fragment>
-      <div>
-        <input type="text" placeholder="ID" onChange={e => setEmail(e.target.value)}/> <br/>
-        <input type="password" placeholder="PW" onChange={e => setPassword(e.target.value)}/> <br/>
-        <button onClick={() => signUp(email, password)}>
-          회원가입
-        </button>
-      </div>
-
-      <hr/>
-
-      <div>
-        <input type="text" ref={idRef} /> <br/>
-        <input type="password" ref={pwRef} /> <br/>
-        <button onClick={() => signin(idRef.current?.value, pwRef.current?.value)}>
-          로그인
-        </button>
-      </div>
-
-      <div>
-        <ul>
-          {users && users.map(user => {
-            return (
-              <li key={user.user_id}>
-                <p>User ID: {user.user_id}</p>
-                <p>User Email: {user.user_email}</p>
-                <p>User Password: {user.user_password}</p>
-                <button onClick={() => deleteSpecificUser(user.user_email, user.user_password)}>삭제</button>
-              </li>
-            )
-          })}
-        </ul>
-      </div>
-    </React.Fragment>
-  )
-}
-
-
-export default App
+export default App;
